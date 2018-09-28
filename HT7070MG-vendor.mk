f880cc13f0d84636d90613a8485247699aef17c8
mehanik6 <mehanik67@bk.ru>
2018-09-29 01:09:30 +0300

    first commit

---
+++ HT7070MG-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/DIGMA/HT7070MG/proprietary/xbin,system/xbin)
+
